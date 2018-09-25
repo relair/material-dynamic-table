a83c77a36977f9b7661837a3c9063e5cf5c18168
Marcin <marcin.suty@copfs.gsi.gov.uk>
2018-09-25 12:25:41 +0100

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ld-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'lib-demo';
+}
